406b73e67402a1306861b53455e23209617d5567
pyliu <3634168+pyliu@users.noreply.github.com>
2021-10-24 23:58:09 +0800

    move logo img center

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <script setup>
 // https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { ref, watchEffect } from 'vue'
-import localforage from 'localforage'
+import { ref, watchEffect } from "vue";
+import localforage from "localforage";
 
-const loggedin = ref(false)
-
+const loggedin = ref(false);
 </script>
 
 <template lang="pug">
-div.p-5: img.w-auto(alt="事務所LOGO", src="./assets/logo_removebg.png")
+section.p-5: img.w-auto.mx-auto(alt="事務所LOGO", src="./assets/logo_removebg.png")
 router-view
 //- div: Login(msg="TEST ELEMENT BUTTON")
 </template>
 
 <script>
 export default {
-  async created () {
-    this.loggedin = await localforage.getItem('session')
+  async created() {
+    this.loggedin = await localforage.getItem("session");
     if (this.loggedin) {
-      this.$router.push('/about')
+      this.$router.push("/about");
     } else {
-      this.$router.push('/')
+      this.$router.push("/");
     }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
