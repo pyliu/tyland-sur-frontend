86d6e70b24373f55164588ed7e82a1b82d2fe73f
pyliu <3634168+pyliu@users.noreply.github.com>
2021-10-30 22:18:36 +0800

    created project use "yarn create nuxt-app <project-name>"

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import router from './router'
-import App from "./App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/packages/theme-chalk/src/index.scss";
-import "./index.css";
-
-const app = createApp(App).use(router).use(ElementPlus).mount("#app");
