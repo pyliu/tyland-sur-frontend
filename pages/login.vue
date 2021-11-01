279810045895feab1db63adfa3df4c0617b9d7f9
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-01 22:32:39 +0800

    test login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,27 +1,75 @@
 <template lang="pug">
 b-container
-  div
-    b-form-group(
-      description="請輸入帳號"
-      label="帳號"
-      label-for="input-account"
-      valid-feedback="感謝您!"
-      :invalid-feedback="invalidFeedback"
-      :state="state"
-    )
-      b-input(id="input-account" v-model="account" :state="state" trim)
+  b-form-group(
+    label="帳號",
+    label-for="input-account",
+    valid-feedback="",
+    :invalid-feedback="accInvalidFeedback",
+    :state="accState"
+  ): b-input#input-account(
+    v-model="account",
+    :state="accState",
+    placeholder="請輸入帳號",
+    trim
+  )
+  b-form-group(
+    label="密碼",
+    label-for="input-password",
+    valid-feedback="",
+    :invalid-feedback="pwInvalidFeedback",
+    :state="pwState"
+  ): b-input#input-password(
+    v-model="password",
+    :state="pwState",
+    placeholder="請輸入密碼",
+    trim
+  )
+  .text-center: b-button(
+    variant="primary"
+    @click="login"
+    :disabled="!pwState || !accState"
+  ) 登入
 </template>
 
 <script>
 export default {
-  watch: {
-    user(val) { console.log(val) }
+  data: () => ({
+    account: "",
+    password: "",
+  }),
+  computed: {
+    accState() {
+      return this.account.length >= 4;
+    },
+    accInvalidFeedback() {
+      if (this.account.length > 0) {
+        return "請輸入至少4個字元";
+      }
+      return "請輸入帳號。";
+    },
+    pwState() {
+      return this.password.length > 0;
+    },
+    pwInvalidFeedback() {
+      return "請輸入密碼。";
+    },
   },
+  watch: {},
   mounted () {
+    console.log(this.$config)
+  },
+  methods: {
+    login () {
+      this.$axios.post('http://localhost:4500/login', {
+        account: this.account,
+        password: this.password
+      }).then(({ data }) => {
+        console.log(data)
+      })
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
