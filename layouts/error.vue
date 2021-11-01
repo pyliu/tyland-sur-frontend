b10ebee101026136b69600b98e75d0caa39c2448
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-01 23:38:08 +0800

    add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+b-container.center
+  h1.d-flex.justify-content-center(v-if="error.statusCode === 404")
+    .text-danger.mr-1 {{ error.statusCode }}
+    span 找不到頁面
+  h1.d-flex.justify-content-center(v-else)
+    .text-danger.mr-1 {{ error.statusCode }}
+    span 有錯誤發生
+  h4 ⚠ {{ error.message }}
+  nuxt-link(to="/") 👉🏻 回到首頁
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'default' // you can set a custom layout for the error page
+}
+</script>
+
+<style lang="scss" scoped>
+.center {
+  margin-top: calc(20vh - 0px);
+  text-align: center;
+}
+</style>
