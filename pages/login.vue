ac4cc339f3bc42287d2510ddf6a17cd75529fe09
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-01 17:56:56 +0800

    add composition-api for testing

---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+b-container
+  div
+    b-form-group(
+      description="請輸入帳號"
+      label="帳號"
+      label-for="input-account"
+      valid-feedback="感謝您!"
+      :invalid-feedback="invalidFeedback"
+      :state="state"
+    )
+      b-input(id="input-account" v-model="account" :state="state" trim)
+</template>
+
+<script>
+export default {
+  watch: {
+    user(val) { console.log(val) }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
