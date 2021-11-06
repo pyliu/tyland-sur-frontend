d6e8f961cfca8a3bb7010d945ef8a51c4ac29da1
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-06 23:51:21 +0800

    update uploader search as same as others

--- pages/search/uploader.vue
+++ pages/search/uploader.vue
@@ -4,9 +4,12 @@ b-card(no-body) UPLOADER
 
 <script>
 export default {
-  emit: ['dataUpdate'],
+  emit: ['data-update'],
   head: {
     title: '使用者條件查詢-界標閱覽系統'
+  },
+  mounted () {
+    this.$emit('data-update', { message: '接收使用者查詢回傳DATA' })
   }
 }
 </script>
