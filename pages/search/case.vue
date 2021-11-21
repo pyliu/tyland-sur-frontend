45bf5470dfddcf175077d5efc9cd8c68dc7db8db
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-21 16:43:56 +0800

    add basic search for case

--- pages/search/case.vue
+++ pages/search/case.vue
@@ -20,13 +20,15 @@ div
       :options="sectionOpts"
     )
     b-input-group(prepend="地號"): b-input(v-model="landNum")
-  b-button(variant="outline-primary", block) 🔎 搜尋
+  b-button(@click="search", variant="outline-primary", block) 🔎 搜尋
   hr
   h5 搜尋結果
   CaseList(:list="searchedData", :loading="isBusy", :per-page="10")
 </template>
 
 <script>
+import isEmpty from 'lodash/isEmpty';
+
 export default {
   emit: ["data-update"],
   head: {
@@ -52,27 +54,80 @@ export default {
     caseId() {
       return `${this.year}${this.code}${this.num}`;
     },
+    formatedLandNum() {
+      if (this.landNum.includes("-")) {
+        const numbers = this.landNum.split("-");
+        const parent = parseInt(numbers[0]);
+        const child = parseInt(numbers[1]);
+        if (parent > 9999 || parent < 0 || child < 0 || child > 9999) {
+          console.warn("地號格式不正確", `${parent}-${child}`);
+          return "";
+        }
+        return ('0000' + parent).slice(-4) + ('0000' + child).slice(-4);
+      } else {
+        const number = parseInt(this.landNum);
+        if (number > 0 && number < 10000) {
+          return ('0000' + number).slice(-4) + "0000";
+        }
+        console.warn("地號格式不正確", `${number}-0000`);
+        return "";
+      }
+    },
+    filter() {
+      const filter = {};
+      !isEmpty(this.year) && (filter.year = ("000" + this.year).slice(-3));
+      !isEmpty(this.code) && (filter.code = this.code);
+      !isEmpty(this.num) && (filter.num = ("000000" + this.num).slice(-6));
+      !isEmpty(this.section) && (filter.section = this.section);
+      !isEmpty(this.opdate) && (filter.opdate = this.opdate);
+      !isEmpty(this.formatedLandNum) && (filter.lands = { number: this.formatedLandNum});
+      return filter;
+    }
   },
   created() {
-    this.opdate = this.today;
+    // codes/sections which is Map structure from global mixin
     this.codeOpts = [{ text: "", value: "" }];
-    this.codes.forEach((element) => {
+    this.codes.forEach((val, key, map) => {
       this.codeOpts.push({
-        text: `${element.value} ${element.text}`,
-        value: element.value,
+        text: `${key} ${val}`,
+        value: key,
       });
     });
     this.sectionOpts = [{ text: "", value: "" }];
-    this.sections.forEach((element) => {
+    this.sections.forEach((val, key, map) => {
       this.sectionOpts.push({
-        text: `${element.value} ${element.text}`,
-        value: element.value,
+        text: `${key} ${val}`,
+        value: key,
       });
     });
   },
   mounted() {
     // this.$emit("data-update", { message: "收到案件查詢回傳DATA" });
   },
+  methods: {
+    search () {
+      console.log("搜尋條件", this.filter);
+      this.isBusy = true;
+      this.searchedData = [];
+      this.$axios
+        .post("/api/search", this.filter)
+        .then(({ data }) => {
+          if (Array.isArray(data.payload)) {
+            this.success(data.message);
+            this.searchedData = [...data.payload];
+            this.$store.commit('wipList', this.searchedData);
+          } else {
+            this.warning(data.message);
+          }
+        })
+        .catch((err) => {
+          console.warn(err);
+        })
+        .finally(() => {
+          this.isBusy = false;
+        });
+    }
+  }
 };
 </script>
 
