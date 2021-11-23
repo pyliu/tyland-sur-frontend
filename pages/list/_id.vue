972c0fa132b653038aff6d47053b01740276022b
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-23 16:59:27 +0800

    wip

--- pages/list/_id.vue
+++ pages/list/_id.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
 div(v-if="dataReady")
-  b-card(:title="formatedCaseId", :sub-title="`立案者：${userMap.get(creator) || creator}`")
+  b-card(
+    :title="formatedCaseId",
+    :sub-title="`立案者：${userMap.get(creator) || creator}`"
+  )
     b-card-text
       b-input-group.my-1(prepend="　　地段"): b-select(
         v-model="caseData.section",
         :options="sectionOpts",
-        :state="sectionOK"
+        :state="sectionOK",
         :disabled="!isOwner"
       )
       b-input-group(prepend="複丈日期"): b-input(
         v-model="caseData.opdate",
         type="date",
         :max="maxOpdate",
-        :state="opdateOK"
+        :state="opdateOK",
         :disabled="!isOwner"
       )
       .d-flex.justify-content-center.mt-2
@@ -27,9 +30,40 @@ div(v-if="dataReady")
           span 修改
   hr
   b-card
-    template(#header): .d-flex.justify-content-between
+    template(#header): .d-flex.justify-content-between.align-items-center
       span 地號
-    b-card-text TODO
+      b-button(variant="outline-light", v-b-modal.add-land-modal): b-icon.mr-2(
+        icon="plus-circle-fill",
+        variant="primary",
+        font-scale="1.25"
+      )
+      b-modal#add-land-modal(
+        ref="add-land-modal",
+        :title="`新增地號 - ${section} - ${formatedCaseId}`"
+      )
+        b-input-group.my-1(prepend="地號"): b-input(
+          id="input-live"
+          v-model="landNum",
+          :state="landNumOK"
+          placeholder="輸入地號 ... "
+          trim
+        )
+        b-form-text.text-right(id="input-live-help") {{ formatedLandNum }}
+        template(#modal-footer="{ ok, cancel, hide }")
+          b-button(
+            @click="addLandNumber",
+            variant="outline-primary",
+            :disabled="!landNumOK"
+          ) 確認
+
+    b-card-text
+      b-list-group(v-if="caseData.lands.length > 0", flush)
+        b-list-group-item(
+          v-for="(land, idx) in caseData.lands",
+          :key="`land_${idx}`"
+        ).
+          AAA
+      .text-center.my-3(v-else) ⚠ 無資料
 .text-center.mt-5(v-else-if="isBusy")
   b-icon(icon="arrow-clockwise", animation="spin-pulse", font-scale="4")
 h4.text-center.mt-5(v-else) ⚠ 找不到資料 ‼
@@ -47,20 +81,54 @@ export default {
     sectionOpts: [],
     maxOpdate: "",
     origSection: "",
-    origOpdate: ""
+    origOpdate: "",
+    landNum: "",
   }),
   computed: {
+    landNumOK() {
+      return !isEmpty(this.formatedLandNum);
+    },
+    formatedLandNum() {
+      if (this.landNum.length > 9) {
+        return "";
+      }
+      if (this.landNum.includes("-")) {
+        const numbers = this.landNum.split("-");
+        if (numbers[0].length > 4) {
+          this.warning(`母號格是不正確 ${number[0]}`);
+          return "";
+        }
+        if (numbers[1].length > 4) {
+          this.warning(`子號格是不正確 ${number[1]}`);
+          return "";
+        }
+        const parent = parseInt(numbers[0]);
+        const child = parseInt(numbers[1]);
+        if (parent > 9999 || parent < 0 || child < 0 || child > 9999) {
+          console.warn("地號格式不正確", `${parent}-${child}`);
+          return "";
+        }
+        return ("0000" + parent).slice(-4) + ("0000" + child).slice(-4);
+      } else {
+        const number = parseInt(this.landNum);
+        if (number > 0 && number < 10000) {
+          return ("0000" + number).slice(-4) + "0000";
+        }
+        console.warn("地號格式不正確", `${number}-0000`);
+        return "";
+      }
+    },
     btnDisabled() {
       if (this.sectionChanged || this.opdateChanged) {
         return !this.ok || this.isBusy;
       }
-      return true
+      return true;
     },
     sectionChanged() {
-      return this.caseData.section !== this.origSection; 
+      return this.caseData.section !== this.origSection;
     },
     opdateChanged() {
-      return this.caseData.opdate !== this.origOpdate; 
+      return this.caseData.opdate !== this.origOpdate;
     },
     sectionOK() {
       return !isEmpty(this.caseData.section);
@@ -171,8 +239,8 @@ export default {
         let found = -1;
         this.wipList.find((val, idx, arr) => {
           val._id === this.caseData._id && (found = idx);
-          return val._id === this.caseData._id
-        })
+          return val._id === this.caseData._id;
+        });
         if (found !== -1) {
           this.wipList[found] = this.caseData;
         }
@@ -184,7 +252,7 @@ export default {
         .post("/api/update", {
           _id: this.caseData._id,
           section: this.caseData.section,
-          opdate: this.caseData.opdate
+          opdate: this.caseData.opdate,
         })
         .then(({ data }) => {
           if (data.statusCode === this.statusCode.SUCCESS) {
@@ -204,6 +272,20 @@ export default {
         .finally(() => {
           this.isBusy = false;
         });
+    },
+    addLandNumber() {
+      if (!Array.isArray(this.caseData.lands)) {
+        this.caseData.lands = [];
+      }
+      const existed = this.caseData.lands.find((element) => {
+        return element.number === this.landNum;
+      });
+      if (existed) {
+        this.warning(`⚠ 地號 ${this.landNum} 已存在。`);
+      } else {
+      }
+      this.landNum = "";
+      this.$refs["add-land-modal"].hide();
     },
   },
 };
