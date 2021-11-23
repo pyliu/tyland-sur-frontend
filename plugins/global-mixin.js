3acf219444662d80b66c8724e0b151c7808f01a7
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-23 23:11:11 +0800

    uuid

--- plugins/global-mixin.js
+++ plugins/global-mixin.js
@@ -55,6 +55,17 @@ Vue.mixin({
   methods: {
     ...mapActions(["checkSession"]),
     ...mapMutations([]),
+    uuid() {
+      var d = Date.now();
+      if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+      }
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+          return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+    },
     now() {
       // e.g. 2020-11-06 13:39:23
       const now = new Date();
