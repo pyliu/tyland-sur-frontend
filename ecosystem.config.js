ae7b3b3aa09af476bb4c04b57a2d5dada998e730
pyliu <3634168+pyliu@users.noreply.github.com>
2021-12-08 22:36:00 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,18 @@ module.exports = {
     name: 'tyland-sur-fe',
     // 分為 cluster 以及 fork 模式
     exec_mode: 'cluster',
-    instances: 1,
+    instances: 2,
     // 執行服務的入口檔案
-    script: './/node_modules/nuxt/bin/nuxt.js',
+    script: './node_modules/nuxt/bin/nuxt.js',
     args: '',
-    out_file: '../tyland-sur-frontend_out.log',
-    error_file: '../tyland-sur-frontend_err.log',
+    out_file: '../logs/tyland-sur-frontend_out.log',
+    error_file: '../logs/tyland-sur-frontend_err.log',
     // 'cron' 模式指定重啟時間，只支持 cluster 模式
     cron_restart: '0 7 * * *',
     // log 顯示時間
     time: true,
     // 適合開發時用，檔案一有變更就會自動重啟
-    watch: true,
+    watch: false,
     ignore_watch: ['[/\\]./', 'node_modules', '.git', 'dist'],
     max_memory_restart: '1024M',
     env: {
