826e8e918ca54805f14807d6187a9396e9bdcee2
pyliu <3634168+pyliu@users.noreply.github.com>
2022-06-29 15:51:52 +0800

    add spin button

--- pages/case/index.vue
+++ pages/case/index.vue
@@ -1,34 +1,56 @@
 <template lang="pug">
 b-card.border-0(no-body)
-  b-card-title 案件列表
+  b-card-title: .d-flex.align-items-center
+    span 案件列表 - 
+    .mx-1 最近
+    b-spinbutton.mx-1(
+      v-model="count",
+      min="10",
+      max="100",
+      inline
+    )
+    span 件
   CaseList(:list="list", :loading="isBusy")
 </template>
 
 <script>
+import debounce from 'lodash/debounce'
 export default {
   head: {
     title: "案件列表-界標閱覽系統"
   },
   data: () => ({
     list: [],
+    count: 20
   }),
   created() {
-    this.clearWipState();
-    this.isBusy = true;
-    this.$axios
-      .post("/api/search/case", { limit: 20, code: `^${this.site}` })
-      .then(({ data }) => {
-        if (Array.isArray(data.payload)) {
-          this.list = [...data.payload];
-        }
-        this.$store.commit('wipList', this.list);
-      })
-      .catch((err) => {
-        console.warn(err);
-      })
-      .finally(() => {
-        this.isBusy = false;
-      });
+    this.load();
+    this.debouncedLoad = debounce(this.load, 500);
+  },
+  watch: {
+    count(dontcare) {
+      this.debouncedLoad();
+    }
+  },
+  methods: {
+    load() {
+      this.clearWipState();
+      this.isBusy = true;
+      this.$axios
+        .post("/api/search/case", { limit: this.count, code: `^${this.site}` })
+        .then(({ data }) => {
+          if (Array.isArray(data.payload)) {
+            this.list = [...data.payload];
+          }
+          this.$store.commit('wipList', this.list);
+        })
+        .catch((err) => {
+          console.warn(err);
+        })
+        .finally(() => {
+          this.isBusy = false;
+        });
+    }
   }
 };
 </script>
