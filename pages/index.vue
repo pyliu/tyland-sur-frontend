c8c047ddea14691c42fc4a378849e28ca5397f63
pyliu <3634168+pyliu@users.noreply.github.com>
2022-08-12 09:38:57 +0800

    wording

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 b-card
   b-card-title.d-flex.align-items-center
-    span.mr-auto 界標閱覽系統
+    span.mr-auto 界標即可拍線上系統開發沿革
     NuxtLink(to="/case" title="開始使用" style="font-size: 3rem") ➭
   b-card-text
     ul(style="padding-left: 1.5rem;")
-      li 土地界標為保障民眾土地財產之重要依據，本所自96年以來就已辦畢之鑑界案件進行界址拍攝作業，並列為案件結案之必要文件之一。
-      li 本網站之開發讓外業同仁能於外業過程中輕鬆地透過手機拍照功能即可上傳界標埋設之相片，或於外業時透過瀏覽網頁搜尋歷次鑑界之界址相片，提升同仁作業效率。
+      li 界標為連結地籍圖及現地之重要標示，亦為保障測量人員測量成果與民眾權益之重要依據，桃園市政府地政局於111年4月7日以桃地測字第1110019472號函附研商本市各地政事務所測量業務精進作為彙整第3次會議紀錄將鑑界拍照程式推廣至轄內各地政事務所。
+      li 本系統由桃園市桃園地政事務所開發，於111年7月由桃園市政府地政局統籌建構於局端，並交各地政事務所測試後於111年9月19日正式上線使用。
     b-img.img-fluid.img-thumbnail(src="~/assets/images/sample.jpg")
 </template>
 
