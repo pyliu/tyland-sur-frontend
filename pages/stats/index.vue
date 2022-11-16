ff926ed0ad7f7a690ecbb4613d008a292122d194
pyliu <3634168+pyliu@users.noreply.github.com>
2022-11-16 17:02:44 +0800

    add stats page

---
+++ pages/stats/index.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+b-card.border-0(no-body)
+  b-card-title: .d-flex.align-items-center 統計
+  
+</template>
+
+<script>
+import debounce from 'lodash/debounce'
+export default {
+  head: {
+    title: "統計-界標即可拍系統"
+  },
+  data: () => ({ }),
+  created() {
+    this.uploadedImageCount();
+  },
+  watch: { },
+  methods: {
+    uploadedImageCount() {
+      this.isBusy = true;
+      this.$axios
+        .get(`/api/stats/${this.site}/uploaded`)
+        .then(({ data }) => {
+          console.warn(data)
+        })
+        .catch((err) => {
+          console.warn(err);
+        })
+        .finally(() => {
+          this.isBusy = false;
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
