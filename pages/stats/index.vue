15838fa8d5371e7fe3a23ce0a8751d482c012614
pyliu <3634168+pyliu@users.noreply.github.com>
2022-11-17 09:30:31 +0800

    add case/mark stats

--- pages/stats/index.vue
+++ pages/stats/index.vue
@@ -1,27 +1,64 @@
 <template lang="pug">
 b-card.border-0(no-body)
-  b-card-title: .d-flex.align-items-center 統計
+  b-card-title: .d-flex.align-items-center {{ site }} 統計數據
+  b-card-body
+    h4 目前建立案件數量：{{ caseCount }}
+    h4 目前建立界標數量：{{ markCount }}
+    h4 目前上傳圖檔數量：{{ imgCount }}
   
 </template>
 
 <script>
-import debounce from 'lodash/debounce'
 export default {
   head: {
-    title: "統計-界標即可拍系統"
+    title: "綜合統計-界標即可拍系統"
   },
-  data: () => ({ }),
+  data: () => ({
+    imgCount: '讀取中',
+    caseCount: '讀取中',
+    markCount: '讀取中'
+  }),
   created() {
-    this.uploadedImageCount();
+    this.loadUploadedImageCount();
+    this.loadCasesCount();
+    this.loadMarksCount();
   },
   watch: { },
   methods: {
-    uploadedImageCount() {
+    loadUploadedImageCount() {
       this.isBusy = true;
       this.$axios
         .get(`/api/stats/${this.site}/uploaded`)
         .then(({ data }) => {
-          console.warn(data)
+          this.imgCount = data.payload
+        })
+        .catch((err) => {
+          console.warn(err);
+        })
+        .finally(() => {
+          this.isBusy = false;
+        });
+    },
+    loadCasesCount() {
+      this.isBusy = true;
+      this.$axios
+        .get(`/api/stats/${this.site}/cases`)
+        .then(({ data }) => {
+          this.caseCount = data.payload
+        })
+        .catch((err) => {
+          console.warn(err);
+        })
+        .finally(() => {
+          this.isBusy = false;
+        });
+    },
+    loadMarksCount() {
+      this.isBusy = true;
+      this.$axios
+        .get(`/api/stats/${this.site}/marks`)
+        .then(({ data }) => {
+          this.markCount = data.payload
         })
         .catch((err) => {
           console.warn(err);
