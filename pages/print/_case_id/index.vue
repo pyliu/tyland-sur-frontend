f68345c4e0067d9965506d06052d0bd2453ec986
pyliu <3634168+pyliu@users.noreply.github.com>
2024-03-25 17:05:41 +0800

    starting to make case print page

--- pages/print/_case_id/index.vue
+++ pages/print/_case_id/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 b-card.border-0(no-body)
-  b-card-title 案件列表
+  b-card-title {{ formatedCaseId(wip) }}
   b-card-sub-title {{ paramCaseId }}
   CaseList.mt-2(:list="list", :loading="isBusy")
 </template>
@@ -8,7 +8,7 @@ b-card.border-0(no-body)
 <script>
 export default {
   head: {
-    title: "案件列表-界標即可拍系統"
+    title: "案件列印-界標即可拍系統"
   },
   layout: 'print',
   data: () => ({
@@ -30,6 +30,24 @@ export default {
   },
   created() {
     this.list = [this.wip];
+  },
+  methods: {
+    caseId(caseData) {
+      if (!caseData.year || !caseData.code || !caseData.num) {
+        this.alert('案件資料有問題，請檢查主控台視窗!');
+        console.warn(caseData);
+        return false;
+      }
+      return ("000" + caseData.year).slice(-3) + '-'
+        + ("XXXX" + caseData.code).slice(-4) + '-'
+        + ("000000" + caseData.num).slice(-6)
+    },
+    formatedCaseId(caseData) {
+      if (this.caseId(caseData)) {
+        return `${("000" + caseData.year).slice(-3)} 年 ${this.codes.get(caseData.code)}(${caseData.code}) 字 ${("000000" + caseData.num).slice(-6)} 號`;
+      }
+      return false;
+    }
   }
 };
 </script>
